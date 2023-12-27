13d3f51d0512bc58f7f8eef19045dc4aad646849
abdellah nejmeddine <crissnajm1998@gmail.com>
2023-12-27 23:49:26 +0100

    J

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './Component/login';
 import Register from './Component/Registre';
+import ProtectedRoute from './Component/ProtectedRoute/ProtectedRoute';
+import { Route, Router, Routes, useLocation } from 'react-router-dom';
 
 function App() {
 
@@ -15,7 +17,7 @@ function App() {
       return null; // Don't render the header on the login page
     }
   
-    return <NavBar/>;
+    return null;
   }
 
   return (
@@ -27,7 +29,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
-        <Route element={<ProtectedRoute />} >
+        <Route element={<ProtectedRoute/>} >
          
 
          
