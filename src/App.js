50e348c2908a59cf1b2e68c8f83430f750397f85
khaoulakanboua <khaoulakanboua@gmail.com>
2023-12-27 22:10:07 +0100

    List

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './Component/login';
 import Register from './Component/Registre';
+import BasicFilterDemo from './Component/ListDemand';
 
 function App() {
   return (
     <div className="App">
-      <Register/>
+      <BasicFilterDemo/>
     </div>
   );
 }
