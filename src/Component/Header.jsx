4ba12425408653d6e4830a5c845e0be3dd2d054e
abdellah nejmeddine <crissnajm1998@gmail.com>
2024-01-03 20:57:10 +0100

    header

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -2,39 +2,61 @@
 
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { FaSignOutAlt } from 'react-icons/fa'; 
+import authService from '../services/auth.service';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const rolesString = localStorage.getItem('role');
+  const isAdmin = rolesString.includes('ROLE_ADMIN');
+  const isUser = rolesString.includes('ROLE_USER') && !isAdmin;
+
+  const logout = () => {
+    authService.logout();
+    navigate('/');
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <Link className="navbar-brand" to="/">Logo</Link>
+        <Link className="navbar-brand" to="/">
+          Logo
+        </Link>
 
-        {/* Utilisation de la classe "me-auto" pour aligner les liens au centre */}
         <div className="collapse navbar-collapse me-auto">
           <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/ListDemande">
-                Demandes
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/ListUserDemande">
-                Mes demandes
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/CreateDemande">
-                Add demande
-              </NavLink>
-            </li>
+            {isAdmin && (
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/ListDemande">
+                  Demandes
+                </NavLink>
+              </li>
+            )}
+            {isUser && (
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/ListUserDemande">
+                  Mes demandes
+                </NavLink>
+              </li>
+            )}
+            {isUser && (
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/CreateDemande">
+                  Add demande
+                </NavLink>
+              </li>
+            )}
           </ul>
         </div>
 
-        {/* Utilisation de la classe "ms-auto" pour aligner le bouton de déconnexion à droite */}
         <div className="ms-auto">
-          {/* Vous devrez remplacer ce bouton par votre bouton de déconnexion */}
-            Logout
+          {rolesString && (
+            <button className="btn btn-outline-light" onClick={logout}>
+              <FaSignOutAlt /> Logout
+            </button>
+          )}
         </div>
       </div>
     </nav>
